23cb2cb3798765e0f994bcba008469fe0db50666
Angelina <Angelina@DESKTOP-ALQRITS>
2022-02-21 14:50:35 -0500

    Add project files.

---
+++ password genorator/Program.cs
@@ -0,0 +1,44 @@
+﻿ using System;
+
+namespace password_genorator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.Write("What is your new password: ");
+            string newPassword = Console.ReadLine();
+            passwordChecker(newPassword);
+
+        }
+
+        private static int passwordChecker(string newPassword)
+        {
+            int result = 0;
+            int totalNum = 0;
+            int totalLetter = 0;
+
+            foreach (char item in newPassword)
+            {
+                if (Char.IsDigit(item))
+                {
+                    totalNum++;
+                }
+
+                if (Char.IsLetter(item))
+                {
+                    totalLetter++;
+                }
+            }
+
+            switch((totalLetter, totalNum))
+            {
+                case ( > 5, > 1):
+                    result = 0;
+            }
+
+            return result;
+        }
+    }
+}
