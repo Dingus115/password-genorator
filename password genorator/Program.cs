27a7ca2c42e2f045cdea1fbcf2e632a5db2a2282
Dingus115 <justinleehannah@gmail.com>
2022-02-21 17:12:16 -0500

    Update
    Changed Language from C# 7.2 to C# 9.0
    
    Allowed case statements.
    Working condition now :)

--- password genorator/Program.cs
+++ password genorator/Program.cs
@@ -4,41 +4,57 @@ namespace password_genorator
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args)                                         //Main method
         {
             
-            Console.Write("What is your new password: ");
+            Console.Write("What is your new password: ");                       //Ask User for new password
             string newPassword = Console.ReadLine();
-            passwordChecker(newPassword);
+            string answer = passwordChecker(newPassword);                          //pass to method-passwordChecker
+            
+
+            Console.WriteLine(answer);
 
         }
 
-        private static int passwordChecker(string newPassword)
+        private static string passwordChecker(string newPassword)                  //take in user input password
         {
-            int result = 0;
+            string result = null;
             int totalNum = 0;
             int totalLetter = 0;
 
-            foreach (char item in newPassword)
+            foreach (char character in newPassword)                                  //check specifications of password ie. letters/numbers
             {
-                if (Char.IsDigit(item))
+                if (Char.IsDigit(character))
                 {
                     totalNum++;
                 }
 
-                if (Char.IsLetter(item))
+                if (Char.IsLetter(character))
                 {
                     totalLetter++;
                 }
             }
 
-            switch((totalLetter, totalNum))
+            switch((totalLetter, totalNum))                                     //check total char to determine acceptance
             {
-                case ( > 5, > 1):
-                    result = 0;
+                case ( >=5, >= 1):
+                    result = "Password has been accepted";
+                    break;
+
+                case ( < 5, >= 1):
+                    result = "Not enough letters! Please try again.";
+                    break;
+
+                case ( >= 5, < 1):
+                    result = "Not enough numbers! Please try again.";
+                    break;
+
+                case ( < 5, < 1):
+                    result = "Not enough letters and numbers! Please try again";
+                    break;
             }
 
-            return result;
+            return result;                                                      //return string to main
         }
     }
 }
